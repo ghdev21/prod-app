b0c63b55e66bd169eada1f07d67bdc775f4ed724
Andrii Yaremenko <you@example.com>
2019-10-30 16:59:09 +0200

    change the order navigation Elements

--- src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -5,8 +5,8 @@ import classes from './NavigationItems.module.scss';
 
 export default (props) => (
   <ul className={classes.Menu}>
-    <NavigationItem title="settings" />
     <NavigationItem title="task-list" />
     <NavigationItem title="statistic" />
+    <NavigationItem title="settings" />
   </ul>
 );
