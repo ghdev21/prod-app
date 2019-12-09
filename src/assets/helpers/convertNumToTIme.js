b17cbdd1d88f454c804a245c1a2ddf6e8dafbfe8
Andrii Yaremenko <you@example.com>
2019-12-09 21:41:20 +0200

    refactor styles

--- src/assets/helpers/convertNumToTIme.js
+++ src/assets/helpers/convertNumToTIme.js
@@ -2,7 +2,9 @@ export default (num) => {
   const hours = Math.floor(num / 60);
   const minutes = num % 60;
   let time = `${hours}h ${minutes} m `;
-  if (hours === 0) time = `${minutes} m`;
+  if (hours === 0) {
+    time = `${minutes} m`;
+  }
   if (minutes === 0) {
     time = `${hours}h`;
   }
--- src/components/TimeLine/TimeLineMark.jsx
+++ src/components/TimeLine/TimeLineMark.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 import classes from './TimeLine.module.scss';
 
 export default ({ text, leftShift, markOpts }) => {
-  const styles = { left: leftShift, position: 'relative' };
   const classNames = [classes.TimeLineMark];
-  if (markOpts) {
-    classNames.push(classes[markOpts]);
-  } else {
-    styles.transform = 'translateX(-50%)';
-    styles.position = 'absolute';
-  }
+  const additionalClass = markOpts ? classes[markOpts] : classes.ShiftedMark;
+  classNames.push(additionalClass);
   return (
-    <p className={classNames.join(' ')} style={styles}>
+    <p className={classNames.join(' ')} style={{ left: leftShift }}>
       <span>{text}</span>
     </p>
   );
