cad2282159b1b87659dadc10e2a7e7bba93141ce
Andrii Yaremenko <you@example.com>
2019-12-19 09:32:25 +0200

    Updates

---
+++ precache-manifest.ef8fc00411d33d8b280280331b1c45ad.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d9f13d45fdf505efb81d4178c1155e",
+    "url": "/prod-app/index.html"
+  },
+  {
+    "revision": "c656617b76aa05ec332e",
+    "url": "/prod-app/static/css/main.b2b35cce.chunk.css"
+  },
+  {
+    "revision": "8d0b2740ae74ba83ca02",
+    "url": "/prod-app/static/js/2.073e3c11.chunk.js"
+  },
+  {
+    "revision": "c656617b76aa05ec332e",
+    "url": "/prod-app/static/js/main.f1d72a43.chunk.js"
+  },
+  {
+    "revision": "8c044fb0a3ccad463d0a",
+    "url": "/prod-app/static/js/runtime-main.342e30d0.js"
+  },
+  {
+    "revision": "73bf16d2b4f56bf72ca356ad5ee39221",
+    "url": "/prod-app/static/media/Logo.73bf16d2.svg"
+  },
+  {
+    "revision": "11913bdc232e6aab44c63cd665a4850b",
+    "url": "/prod-app/static/media/icomoon.11913bdc.ttf"
+  },
+  {
+    "revision": "58c8d665c4ad9f4737ed7d55e019dec7",
+    "url": "/prod-app/static/media/icomoon.58c8d665.eot"
+  },
+  {
+    "revision": "609d18ca0c181b1bf44b51b236a37f94",
+    "url": "/prod-app/static/media/icomoon.609d18ca.svg"
+  },
+  {
+    "revision": "d91a484e0ee1cc34927bfbc3caae0aec",
+    "url": "/prod-app/static/media/icomoon.d91a484e.woff"
+  },
+  {
+    "revision": "586decf04a390e12d2688a8868f86e32",
+    "url": "/prod-app/static/media/pt_sans-web-bold.586decf0.eot"
+  },
+  {
+    "revision": "872e8ada6d5d26c045f7dd37f457311d",
+    "url": "/prod-app/static/media/pt_sans-web-bold.872e8ada.ttf"
+  },
+  {
+    "revision": "cca927d9a16e16ead74d5350f1259d78",
+    "url": "/prod-app/static/media/pt_sans-web-bold.cca927d9.woff"
+  },
+  {
+    "revision": "f4f9b3eb9ed33c772708daec7cbbd09e",
+    "url": "/prod-app/static/media/pt_sans-web-bold.f4f9b3eb.svg"
+  },
+  {
+    "revision": "325283729353f07fa76c0613f22801a3",
+    "url": "/prod-app/static/media/pt_sans-web-regular.32528372.woff"
+  },
+  {
+    "revision": "3c5e4c054d062adfcd61fc969a20e80d",
+    "url": "/prod-app/static/media/pt_sans-web-regular.3c5e4c05.ttf"
+  },
+  {
+    "revision": "8ec8e2ddb6c8cabcbfa30d3e33369b99",
+    "url": "/prod-app/static/media/pt_sans-web-regular.8ec8e2dd.eot"
+  },
+  {
+    "revision": "bd7294384b8f86b69426ff6e4e3beb18",
+    "url": "/prod-app/static/media/pt_sans-web-regular.bd729438.svg"
+  },
+  {
+    "revision": "0e53a4b6c437409a84158df760f00a45",
+    "url": "/prod-app/static/media/roboto-bold.0e53a4b6.woff"
+  },
+  {
+    "revision": "53b54169366df2327e0c43d6333ecc71",
+    "url": "/prod-app/static/media/roboto-bold.53b54169.eot"
+  },
+  {
+    "revision": "872e68c60f1054534f7aa5e087c06007",
+    "url": "/prod-app/static/media/roboto-bold.872e68c6.ttf"
+  },
+  {
+    "revision": "e6fd5ba8cc8edfd33215700511622c21",
+    "url": "/prod-app/static/media/roboto-bold.e6fd5ba8.svg"
+  },
+  {
+    "revision": "06009717d66205a655f7f2d56ce31b14",
+    "url": "/prod-app/static/media/roboto-light.06009717.eot"
+  },
+  {
+    "revision": "13c8d67d36d9f750a8e4e5f9a484fa93",
+    "url": "/prod-app/static/media/roboto-light.13c8d67d.svg"
+  },
+  {
+    "revision": "1fc4350a2a2e639f4fe939dc980244e4",
+    "url": "/prod-app/static/media/roboto-light.1fc4350a.ttf"
+  },
+  {
+    "revision": "8928f5c27d4e33b44ba99e6940a85481",
+    "url": "/prod-app/static/media/roboto-light.8928f5c2.woff"
+  },
+  {
+    "revision": "149d2e68bd4fde635d7955b5dce87126",
+    "url": "/prod-app/static/media/roboto-medium.149d2e68.eot"
+  },
+  {
+    "revision": "2eb373b1d666c949640e564e6d46f752",
+    "url": "/prod-app/static/media/roboto-medium.2eb373b1.woff"
+  },
+  {
+    "revision": "4587a0c7ae09875f15800593b1f47407",
+    "url": "/prod-app/static/media/roboto-medium.4587a0c7.svg"
+  },
+  {
+    "revision": "ace508ffec1c665a9384a8c295381ba3",
+    "url": "/prod-app/static/media/roboto-medium.ace508ff.ttf"
+  },
+  {
+    "revision": "9d9d4085d257ac3772fe2323da8684b9",
+    "url": "/prod-app/static/media/roboto-regular.9d9d4085.svg"
+  },
+  {
+    "revision": "9eacc052ebafbc5f898a39495d9923d2",
+    "url": "/prod-app/static/media/roboto-regular.9eacc052.eot"
+  },
+  {
+    "revision": "b6499374583ff458daa319e3652cd264",
+    "url": "/prod-app/static/media/roboto-regular.b6499374.ttf"
+  },
+  {
+    "revision": "ff3e98b798c5a3c8c17751692e5b575d",
+    "url": "/prod-app/static/media/roboto-regular.ff3e98b7.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d2ed21774b4984c91cdf88f60d8a2ae.js"
+  "/prod-app/precache-manifest.ef8fc00411d33d8b280280331b1c45ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prod-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
